69643c724944b8eda3a81c7663d7b78eede4d0ff
pravin372001 <jothipravin4@gmail.com>
2024-07-23 21:34:11 +0530

    minor  change

--- ZNews/app/src/main/java/com/example/znews/fragments/NewsListFragment.kt
+++ ZNews/app/src/main/java/com/example/znews/fragments/NewsListFragment.kt
@@ -1,5 +1,6 @@
 package com.example.znews.fragments
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +8,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.example.znews.MainActivity
 import com.example.znews.R
 import com.example.znews.databinding.FragmentNewsListBinding
 import com.example.znews.viewmodel.NewsViewModel
@@ -20,6 +22,14 @@ class NewsListFragment : Fragment() {
 
     private var _binding: FragmentNewsListBinding? = null
     private val binding get() = _binding!!
+    private lateinit var mainActivity: MainActivity
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is MainActivity){
+            mainActivity = context
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,6 +40,8 @@ class NewsListFragment : Fragment() {
         viewModel.user.observe(viewLifecycleOwner) { user ->
             binding.userName.text = user?.displayName
         }
+        val toolBar = binding.listToolBar
+        mainActivity.setSupportActionBar(toolBar)
         binding.signOut.setOnClickListener {
             viewModel.logout()
             findNavController().navigate(R.id.action_newsListFragment_to_loginFragment)
